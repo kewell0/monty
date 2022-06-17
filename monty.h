dbb93c99d5cea129507f62c9c0fdf11e39b307a2
kewell0 <63118280+kewell0@users.noreply.github.com>
2022-06-17 02:07:11 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -42,7 +42,7 @@ extern char *push_data;
 char *push_data;
 
 /* Function Prototypes */
-char **tokenize(char *s);
+char **split_line(char *line)
 void (*get_op_func(char *s))(stack_t **stack, unsigned int line_number);
 int stack_len(stack_t **stack);
 void free_stack(stack_t *stack);
